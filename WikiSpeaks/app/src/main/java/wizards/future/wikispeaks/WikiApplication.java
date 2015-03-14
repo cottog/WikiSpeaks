b6051682c6c0860fb960c192adc3bf1084137625
Shane Geller <shanemgeller@gmail.com>
2015-03-14 01:48:31 -0400

    Start Menu Resizing Changes

--- WikiSpeaks/app/src/main/java/wizards/future/wikispeaks/WikiApplication.java
+++ WikiSpeaks/app/src/main/java/wizards/future/wikispeaks/WikiApplication.java
@@ -7,17 +7,24 @@ import android.app.Application;
  * Created by Shane on 3/13/2015.
  */
 public class WikiApplication extends Application {
-    private Activity currentActivity = null;
-
-
+    private Activity mCurrentActivity = null;
+    private boolean mHandsFree = false;
 
     public void onCreate(){
         super.onCreate();
 
     }
 
-    public synchronized void setCurrentActivity(Activity currentActivity){
-        this.currentActivity = currentActivity;
+    public synchronized void setCurrentActivity(Activity mCurrentActivity){
+        this.mCurrentActivity = mCurrentActivity;
+    }
+
+    public boolean isHandsFree(){
+        return mHandsFree;
+    }
+
+    public void setHandsFree(boolean mHandsFree){
+        this.mHandsFree = mHandsFree;
     }
 
 
--- WikiSpeaks/app/src/main/java/wizards/future/wikispeaks/wizards/future/wikispeaks/activities/WikiStartActivity.java
+++ WikiSpeaks/app/src/main/java/wizards/future/wikispeaks/wizards/future/wikispeaks/activities/WikiStartActivity.java
@@ -4,19 +4,41 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import wizards.future.wikispeaks.R;
+import wizards.future.wikispeaks.WikiApplication;
 
 public class WikiStartActivity extends ActionBarActivity {
+    private WikiApplication mApplication = null;
+    private ImageButton mHandsFreeButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiki_start);
+
+        mApplication = (WikiApplication) getApplication();
+        mApplication.setCurrentActivity(this);
+
+        mHandsFreeButton = (ImageButton)findViewById(R.id.handsButton);
+        if(!mApplication.isHandsFree()){
+            mHandsFreeButton.setBackgroundResource(R.drawable.wikispeakshandsfree);
+        }
     }
 
     public void startButtonHandler(View v){
         Intent intent = new Intent(this,WikiChooseActivity.class);
         startActivity(intent);
+    }
+
+    public void handsFreeButtonHandler(View v){
+        if(!mApplication.isHandsFree()){
+            mHandsFreeButton.setBackgroundResource(R.drawable.wikispeakshandsfreeon);
+            mApplication.setHandsFree(true);
+        }else{
+            mHandsFreeButton.setBackgroundResource(R.drawable.wikispeakshandsfree);
+            mApplication.setHandsFree(false);
+        }
     }
 }
