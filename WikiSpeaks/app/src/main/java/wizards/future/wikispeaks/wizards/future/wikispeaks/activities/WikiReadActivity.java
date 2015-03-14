5826e7bb7297b0da119ccc5f6bcd64d9b1aea7aa
mburgunder <mitchellburgunder@yahoo.com>
2015-03-14 08:18:58 -0400

    one thread with everything in it as well as TtS. what are we doing, its after 8

--- WikiSpeaks/app/src/main/java/wizards/future/wikispeaks/wizards/future/wikispeaks/activities/WikiReadActivity.java
+++ WikiSpeaks/app/src/main/java/wizards/future/wikispeaks/wizards/future/wikispeaks/activities/WikiReadActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.speech.tts.TextToSpeech;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,17 +15,20 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import wizards.future.wikispeaks.R;
 
-public class WikiReadActivity extends ActionBarActivity {
+public class WikiReadActivity extends ActionBarActivity implements TextToSpeech.OnInitListener {
     private String mArticleTitle;
     private String EXTRA_MESSAGE = "wizards.future.wikispeaks";
+    private TextToSpeech tts;
     TextView testText;
     String[] mArticleList;
     @Override
@@ -34,10 +38,77 @@ public class WikiReadActivity extends ActionBarActivity {
         Intent intent = getIntent();
         mArticleTitle = intent.getStringExtra(EXTRA_MESSAGE);
         testText = (TextView) findViewById(R.id.testText);
+
+        tts = new TextToSpeech(this,this);
         //testText.setText(mArticleTitle);
 
-        getNumSections();
-        pullFromNetwork(0);
+        //getNumSections();
+        //pullFromNetwork(0);
+        doSomethingDifferent();
+    }
+
+    public void doSomethingDifferent(){
+        Thread wikiThread = new Thread(new Runnable(){
+            public void run(){
+
+                if(mArticleTitle.length() < 0){
+                    //can't find an article with an empty title
+                    return;
+                }
+                URL url;
+                BufferedReader reader = null;
+                StringBuilder builder = new StringBuilder();
+                try{
+                    String urlString = "http://en.wikipedia.org/w/api.php?format=json&action=parse&prop=wikitext&redirects&page=" + mArticleTitle;
+                    url = new URL(urlString);
+                }
+                catch(MalformedURLException e){
+                    //bad URL format
+                    e.printStackTrace();
+                    return;
+                }
+                //reading the file
+                try{
+                    reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        builder.append(line);
+                    }
+                }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+                finally{
+                    if(reader != null){
+                        try{
+                            reader.close();
+                        }
+                        catch(IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                String jsonString = builder.toString();
+                try{
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    jsonString = jsonObject.getString("wikitext");
+                }
+                catch(JSONException e){
+                    e.printStackTrace();
+                }
+
+
+                if (!tts.isSpeaking()){
+                    tts.speak(jsonString,TextToSpeech.QUEUE_FLUSH, null);
+                }
+                //text to speech goes here
+
+            }
+
+
+        });
+        wikiThread.start();
     }
 
     public void getNumSections(){
@@ -61,79 +132,78 @@ public class WikiReadActivity extends ActionBarActivity {
                 BufferedReader reader = null;
                 StringBuilder builder = new StringBuilder();
                 int numSections = 0;
-                while(true) {
-                    try {
-                        String urlString = "http://en.wikipedia.org/w/api.php?format=json&action=parse&prop=wikitext&redirects&page=" + mArticleTitle
-                                + "&section=" + numSections;
-                        url = new URL(urlString);
-                    } catch (MalformedURLException e) {
-                        //bad URL format
-                        e.printStackTrace();
-                        return;
-                    }
-                    //reading the file
-                    try {
-                        reader = new BufferedReader(new InputStreamReader(url.openStream()));
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            builder.append(line);
+                try {
+                    String urlString = "http://en.wikipedia.org/w/api.php?format=json&action=parse&prop=wikitext&redirects&page=" + mArticleTitle
+                            + "&section=" + numSections;
+                    url = new URL(urlString);
+                } catch (MalformedURLException e) {
+                    //bad URL format
+                    e.printStackTrace();
+                    return;
+                }
+                //reading the file
+                try {
+                    reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        String REGEX = ".*nosuchsection.*";
+                        Pattern p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
+                        Matcher m = p.matcher(line); // get a matcher object
+                        boolean noSectionBoolean = m.matches();
+                        if(noSectionBoolean){
+                            break;
                         }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        if (reader != null) {
-                            try {
-                                reader.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+
+                        REGEX = ".*==See Also==.*";
+                        p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
+                        m = p.matcher(line);
+                        boolean seeAlsoBoolean = m.matches();
+                        if(seeAlsoBoolean){
+                            break;
+                        }
+
+                        REGEX = ".*==References==.*";
+                        p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
+                        m = p.matcher(line);
+                        boolean referencesBoolean = m.matches();
+                        if(referencesBoolean){
+                            break;
+                        }
+
+                        REGEX = ".*==Bibliography==.*";
+                        p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
+                        m = p.matcher(line);
+                        boolean bibliographyBoolean = m.matches();
+                        if(bibliographyBoolean){
+                            break;
+                        }
+
+                        REGEX = ".*==External Links==.*";
+                        p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
+                        m = p.matcher(line);
+                        boolean externalLinksBoolean = m.matches();
+                        if(externalLinksBoolean){
+                            break;
+                        }
+                        numSections++;
+                        builder.append(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
+                }
 
                     String JSON = builder.toString();
 
                     //begin matching
-                    String REGEX = ".*nosuchsection.*";
-                    Pattern p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
-                    Matcher m = p.matcher(JSON); // get a matcher object
-                    boolean noSectionBoolean = m.matches();
-                    if(noSectionBoolean){
-                        break;
-                    }
 
-                    REGEX = ".*==See Also==.*";
-                    p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
-                    m = p.matcher(JSON);
-                    boolean seeAlsoBoolean = m.matches();
-                    if(seeAlsoBoolean){
-                        break;
-                    }
-
-                    REGEX = ".*==References==.*";
-                    p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
-                    m = p.matcher(JSON);
-                    boolean referencesBoolean = m.matches();
-                    if(referencesBoolean){
-                        break;
-                    }
-
-                    REGEX = ".*==Bibliography==.*";
-                    p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
-                    m = p.matcher(JSON);
-                    boolean bibliographyBoolean = m.matches();
-                    if(bibliographyBoolean){
-                        break;
-                    }
-
-                    REGEX = ".*==External Links==.*";
-                    p = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
-                    m = p.matcher(JSON);
-                    boolean externalLinksBoolean = m.matches();
-                    if(externalLinksBoolean){
-                        break;
-                    }
-                    numSections++;
-                }
 
                 final int numSectionsFinal = numSections;
                 runOnUiThread(new Runnable(){
@@ -248,5 +318,15 @@ public class WikiReadActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS ){
+            tts.setLanguage(Locale.getDefault());
+        } else {
+            tts = null;
+            Toast.makeText(this, "Failed to initialize", Toast.LENGTH_SHORT).show();
+        }
     }
 }
