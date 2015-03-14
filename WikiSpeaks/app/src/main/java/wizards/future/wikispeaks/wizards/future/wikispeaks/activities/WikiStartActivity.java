ded46a19cbbc9b2973554269b2709a64c4bd5314
Shane Geller <shanemgeller@gmail.com>
2015-03-14 02:19:00 -0400

    UI Improvments

--- WikiSpeaks/app/src/main/java/wizards/future/wikispeaks/wizards/future/wikispeaks/activities/WikiStartActivity.java
+++ WikiSpeaks/app/src/main/java/wizards/future/wikispeaks/wizards/future/wikispeaks/activities/WikiStartActivity.java
@@ -11,7 +11,6 @@ import wizards.future.wikispeaks.WikiApplication;
 
 public class WikiStartActivity extends ActionBarActivity {
     private WikiApplication mApplication = null;
-    private ImageButton mHandsFreeButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +19,10 @@ public class WikiStartActivity extends ActionBarActivity {
 
         mApplication = (WikiApplication) getApplication();
         mApplication.setCurrentActivity(this);
-
-        mHandsFreeButton = (ImageButton)findViewById(R.id.handsButton);
-        if(!mApplication.isHandsFree()){
-            mHandsFreeButton.setBackgroundResource(R.drawable.wikispeakshandsfree);
-        }
     }
 
     public void startButtonHandler(View v){
         Intent intent = new Intent(this,WikiChooseActivity.class);
         startActivity(intent);
-    }
-
-    public void handsFreeButtonHandler(View v){
-        if(!mApplication.isHandsFree()){
-            mHandsFreeButton.setBackgroundResource(R.drawable.wikispeakshandsfreeon);
-            mApplication.setHandsFree(true);
-        }else{
-            mHandsFreeButton.setBackgroundResource(R.drawable.wikispeakshandsfree);
-            mApplication.setHandsFree(false);
-        }
     }
 }
